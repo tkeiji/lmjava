7d1375fb790dcb9eabd774fd0efe819b64c8789c
Keiji Takeda <tkeiji2@gmail.com>
2012-10-25 16:03:37 +0900

    Added Bus Class

--- Bustime.java
+++ Bustime.java
@@ -4,35 +4,63 @@ import java.util.*;
 public class Bustime {
 	public static void main (String[] args){
 
+		ArrayList<Bus> table = new ArrayList<Bus>();
+		tableRead ("regbus.txt", true, table );
+		
+		int len = table.size();
+		for( int i=0; i< len; i++ ){
+			System.out.println( table.get(i) );
+		}
+	}
+	public static void tableRead (String filename, boolean twin, ArrayList<Bus> table ){
+		Bus busdata;
 		Calendar today = Calendar.getInstance();
-
-		ArrayList<Calendar> table = new ArrayList<Calendar>();
-		Calendar bustime;
 		
 		try{
-			FileReader f = new FileReader("regbus.txt");
+			FileReader f = new FileReader( filename );
 			BufferedReader b = new BufferedReader(f);
 			String line;
-			String[] hm;
 			while(( line = b.readLine()) != null ){
-				hm = line.split(":");				
-				bustime = (Calendar)today.clone();
-				bustime.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hm[0]) );
-				bustime.set(Calendar.MINUTE, Integer.parseInt(hm[1]) );
-				bustime.set(Calendar.SECOND, 0);
-
-				table.add(bustime);
+				busdata = new Bus(line, twin, today);
+				table.add(busdata);
 			}
 		}catch(Exception e){
 			System.out.println("File Access Error");
 		}
-		int len = table.size();
-		for( int i=0; i< len; i++ ){
-			System.out.println( 
-				table.get(i).get(Calendar.HOUR_OF_DAY)+
-				":"+
-				table.get(i).get(Calendar.MINUTE)
-			);
-		}
 	}
 }
+class Bus {
+//  implements Comparable
+	private Calendar time;
+	private boolean twin;
+	
+	public Bus( String timeString, boolean twinliner, Calendar today ) {
+		String[] hm;
+		hm = timeString.split(":");				
+		this.time = (Calendar)today.clone();
+		this.time.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hm[0]) );
+		this.time.set(Calendar.MINUTE, Integer.parseInt(hm[1]) );
+		this.time.set(Calendar.SECOND, 0);
+		this.twin = twinliner;
+	}
+	public Calendar getTime(){
+		return time;
+	}
+	public String toString() {
+		if( twin == true )
+			return time.get(Calendar.HOUR_OF_DAY)+
+				":"+
+				time.get(Calendar.MINUTE)+
+				" twinliner";
+		else
+			return time.get(Calendar.HOUR_OF_DAY)+
+				":"+
+				time.get(Calendar.MINUTE)+
+				" twinliner";
+	}
+	public int compareTo(Object obj) {
+		return this.time.compareTo(((Bus)obj).getTime());
+	}
+	
+}
+
